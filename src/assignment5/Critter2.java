0846e0165a2ccca9a43729c3fef8ac1fa0e0aec9
GeorgeZhang98 <32987213+GeorgeZhang98@users.noreply.github.com>
2018-04-09 13:51:11 -0500

    Put Files
    Put files on Git

---
+++ src/assignment5/Critter2.java
@@ -0,0 +1,61 @@
+/*
+ * CRITTERS GUI Main.java
+ * EE422C Project 5 submission by
+ * George Zhang
+ * jz22349
+ * 15055
+ * Slip days used: <0>
+ * Spring 2018
+ */
+
+package assignment5;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Critter1 is a type of critter that only moves in the west directions
+ * It  fights 66% of the time
+ * it reproduces when it has more than a 100 energy
+ */
+
+public class Critter2 extends Critter {
+
+    private int dir;
+
+    public Critter2() { dir = Critter.getRandomInt(4) + 3; }
+    /** toString method
+     * @return character to be printed
+     */
+    @Override
+    public String toString() { return "2"; }
+    /** fight method
+     * @param String not_used
+     * @return boolean which indicates how critter will handle conflict
+     */
+    public boolean fight(String not_used) {int a = Critter.getRandomInt(3);String b = look(dir,true); return a > 0; }
+    /** do TimeStep method
+     *
+     */
+    @Override
+    public void doTimeStep() {
+
+		/* Take two steps forward */
+        run(dir);
+
+		/* Check if able to reproduce */
+        if (getEnergy() > 100) {
+            Critter2 child = new Critter2();
+            reproduce(child, Critter.getRandomInt(4) + 3);
+        }
+
+		/* Update direction */
+        dir = Critter.getRandomInt(4) + 3;
+    }
+    @Override
+
+    public CritterShape viewShape() { return CritterShape.STAR;}
+    @Override
+    public javafx.scene.paint.Color viewFillColor() { return javafx.scene.paint.Color.MIDNIGHTBLUE; }
+    @Override
+    public javafx.scene.paint.Color viewOutlineColor() { return javafx.scene.paint.Color.CHOCOLATE; }
+}
